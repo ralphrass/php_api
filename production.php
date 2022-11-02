3ae32294e1e10e034f7731c917e0859a6959e96b
Ralph Rassweiler <ralphrass@gmail.com>
2022-11-02 17:22:33 -0300

    Add files via upload

---
+++ production.php
@@ -0,0 +1,18 @@
+<?php
+/*
+Constantes de configuração de ambiente de produção.
+*/
+
+// Banco de dados
+const DB_HOST = 'pg.ralph.com.br';
+const DB_PORT = 5432;
+const DB_USER = 'ralph';
+const DB_PASSWORD = 'senhaDoRalphPg';
+const DB_DATABASE = 'ralph';
+
+// Envio de email
+const MAIL_HOST = 'smtp.ralph.com.br';
+const MAIL_USERNAME = 'no-reply@ralph.com.br';
+const MAIL_PASSWORD = 'outraSenhaDoRalph';
+const MAIL_PORT = 587;
+const MAIL_FROM = 'API do Ralph';
